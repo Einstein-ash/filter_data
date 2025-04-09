c63f29d6bd10ba3d3585a3441c8585db7dd6ccc1
Ashwani <ashwanisingh46572@gmail.com>
2025-04-09 11:38:16 +0530

    camera check2

--- src/pages/Scanner.jsx
+++ src/pages/Scanner.jsx
@@ -5,18 +5,36 @@ const Scanner = () => {
 
       const videoRef = useRef(null);
     
-      useEffect(() => {
-        // Access the camera
-        navigator.mediaDevices.getUserMedia({ video: true })
-          .then(stream => {
+    //   useEffect(() => {
+    //     // Access the camera
+    //     navigator.mediaDevices.getUserMedia({ video: true })
+    //       .then(stream => {
+    //         if (videoRef.current) {
+    //           videoRef.current.srcObject = stream;
+    //         }
+    //       })
+    //       .catch(err => {
+    //         console.error("Camera access denied:", err);
+    //       });
+    //   }, []);
+
+
+    useEffect(() => {
+        const getCamera = async () => {
+          try {
+            const stream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment' } });
             if (videoRef.current) {
               videoRef.current.srcObject = stream;
             }
-          })
-          .catch(err => {
-            console.error("Camera access denied:", err);
-          });
+          } catch (err) {
+            console.error("Camera access error:", err);
+            alert("Camera permission is required to use this feature. Please allow it in your browser settings.");
+          }
+        };
+      
+        getCamera();
       }, []);
+      
 
 
   return (
