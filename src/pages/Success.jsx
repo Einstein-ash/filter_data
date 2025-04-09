37a854ccafbe619b8ef4aeaceedb6228314e44a2
Ashwani <ashwanisingh46572@gmail.com>
2025-04-09 19:54:30 +0530

    success online video link check 2

--- src/pages/Success.jsx
+++ src/pages/Success.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import './Success.css';
-import giff from "../assets/images/success_gif.gif"
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
+// import giff from "../assets/images/success_gif.gif"
 
 const Success = () => {
   return (
     <div className="success-container">
-      <img src={giff} alt="" />
+
+    <DotLottieReact
+      src="https://lottie.host/fc5081aa-982a-485d-a6b1-2f5aafeb407e/ixBHyon6J5.lottie"
+      loop
+      autoplay
+    />
+
+      {/* <img src={giff}  alt="" /> */}
+      
     </div>
+    
   );
 };
 
 export default Success;
+
